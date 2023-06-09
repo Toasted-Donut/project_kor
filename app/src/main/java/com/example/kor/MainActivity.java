793a7390ad784c913726daa4de6d33ce4b583f63
Toasted-Donut <Toasted-Donut@yandex.ru>
2023-06-09 11:58:34 +0300

    0.1

--- app/src/main/java/com/example/kor/MainActivity.java
+++ app/src/main/java/com/example/kor/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.kor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.navigation.Navigation;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -41,83 +43,27 @@ public class MainActivity extends AppCompatActivity {
     BluetoothLeScanner mLeScanner;
     BluetoothGattService mBluetoothGattService;
     BluetoothGattCharacteristic mBluetoothGattCharacteristic;
-    private boolean mScanning = false;
+    public boolean mScanning = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH},111);
+        }
         //scan
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
-        scanLeDevice(!mScanning);
-        Button btn = findViewById(R.id.btn_onoff);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                byte[] a = new byte[]{0x04, 0x00, 0x02, 0x02};
-                mBluetoothGattCharacteristic.setValue(a);
-                Log.wtf("lala",String.valueOf(mGatt.writeCharacteristic(mBluetoothGattCharacteristic)));
-                Log.v("lala",Arrays.toString(mBluetoothGattCharacteristic.getValue()));
-              //  Log.v("lala",String.valueOf( mGatt.readCharacteristic(mBluetoothGattCharacteristic)));
-               // Log.wtf("lala", Arrays.toString(mBluetoothGattCharacteristic.getValue()));
-
-               // Log.v("lala",String.valueOf(mGatt.readCharacteristic(mBluetoothGattCharacteristic)));
-
-            }
-        });
+        //scanLeDevice(!mScanning);
 
 
-        SeekBar bar = findViewById(R.id.bar);
-        SeekBar bar2 = findViewById(R.id.bar2);
-        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                //Log.w("lala",String.valueOf(i));
 
-                byte v = (byte)(-i < 0 ? (255 - i) : -i);
-                byte[] a = new byte[]{8, 0x00, (byte)0x81, 0x01, 0x11, 0x51, 0x00, v};
-                mBluetoothGattCharacteristic.setValue(a);
-                mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
 
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                seekBar.setProgress(0);
-                byte[] a = new byte[]{8, 0x00, (byte)0x81, 0x01, 0x11, 0x51, 0x00, 0x00};
-                mBluetoothGattCharacteristic.setValue(a);
-                mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
-            }
-        });
-        bar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int angle = normalizeAngle(i);
-                byte[] a = new byte[]{0x0E, 0x00, (byte)0x81, 0x00, 0x11, 0x0D, (byte)(angle & 0xff), (byte)((angle >> 8) & 0xff), (byte)((angle >> 16) & 0xff), (byte)((angle >> 24) & 0xff), 0x32, 0x32, 0x7E, 0x00};
-                mBluetoothGattCharacteristic.setValue(a);
-                mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                seekBar.setProgress(0);
-                byte[] a = new byte[]{0x0E, 0x00, (byte)0x81, 0x00, 0x11, 0x0D, 0x00, 0x00, 0x00, 0x00, 0x32, 0x32, 0x7E, 0x00};
-                mBluetoothGattCharacteristic.setValue(a);
-                mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
-            }
-        });
 
     }
+
     private final ScanCallback scanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
@@ -125,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
             BluetoothDevice device = result.getDevice();
 
             Log.wtf("lala", device.getName());
-            mGatt = device.connectGatt(getApplicationContext(),false,bluetoothGattCallback);
+            mGatt = device.connectGatt(getApplicationContext(), false, bluetoothGattCallback);
         }
     };
     private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
@@ -133,16 +79,17 @@ public class MainActivity extends AppCompatActivity {
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             super.onConnectionStateChange(gatt, status, newState);
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                Log.w("lala","connected");
+                Log.w("lala", "connected");
                 gatt.discoverServices();
             } else {
-               // mGatt.close();
+                // mGatt.close();
             }
         }
+
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             super.onServicesDiscovered(gatt, status);
-            Log.i("lala","services discovered");
+            Log.i("lala", "services discovered");
             mBluetoothGattService = mGatt.getService(SERVICE_UUID);
             mBluetoothGattCharacteristic = mBluetoothGattService.getCharacteristic(CHARACTERISTIC_UUID);
             mBluetoothGattCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
@@ -153,32 +100,33 @@ public class MainActivity extends AppCompatActivity {
             mGatt.writeDescriptor(gattDescriptor);
             //Log.wtf("lala", String.valueOf(mBluetoothGattCharacteristic.getValue()));
             //Log.i("lala",String.valueOf(mGatt.readCharacteristic(mBluetoothGattCharacteristic)));
-
+            Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment).navigate(R.id.drive_frag);
         }
 
         @Override
         public void onCharacteristicRead(@NonNull BluetoothGatt gatt, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value, int status) {
             super.onCharacteristicRead(gatt, characteristic, value, status);
-            Log.wtf("lala","read");
+            Log.wtf("lala", "read");
         }
 
         @Override
         public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             super.onCharacteristicWrite(gatt, characteristic, status);
-            Log.wtf("lala","written");
+            Log.wtf("lala", "written");
         }
 
         @Override
         public void onCharacteristicChanged(@NonNull BluetoothGatt gatt, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value) {
             super.onCharacteristicChanged(gatt, characteristic, value);
-            Log.wtf("lala","changed");
+            Log.wtf("lala", "changed");
         }
     };
-    private void scanLeDevice(final boolean enable){
-        if(enable){
+
+    public void scanLeDevice(final boolean enable) {
+        if (enable) {
             UUID[] serviceUUIDs = new UUID[]{SERVICE_UUID};
             List<ScanFilter> filters = null;
-            if(serviceUUIDs != null) {
+            if (serviceUUIDs != null) {
                 filters = new ArrayList<>();
                 for (UUID serviceUUID : serviceUUIDs) {
                     ScanFilter filter = new ScanFilter.Builder()
@@ -194,9 +142,9 @@ public class MainActivity extends AppCompatActivity {
                     .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
                     .setReportDelay(0L)
                     .build();
-            if (mLeScanner != null){
+            if (mLeScanner != null) {
                 mScanning = true;
-                mLeScanner.startScan(filters,scanSettings,scanCallback);
+                mLeScanner.startScan(filters, scanSettings, scanCallback);
                 Log.d("lala", "scan started");
             } else {
                 Log.e("lala", "could not get scanner object");
@@ -206,16 +154,43 @@ public class MainActivity extends AppCompatActivity {
             mLeScanner.stopScan(scanCallback);
         }
     }
-    private int normalizeAngle(int angle){
-        if (angle >= 180)
-        {
+
+    private int normalizeAngle(int angle) {
+        if (angle >= 180) {
             return angle - (360 * ((angle + 180) / 360));
-        }
-        else if (angle < -180)
-        {
+        } else if (angle < -180) {
             return angle + (360 * ((180 - angle) / 360));
         }
 
         return angle;
+    }
+
+    public void disconnectHub() {
+        byte[] a = new byte[]{0x04, 0x00, 0x02, 0x02};
+        mBluetoothGattCharacteristic.setValue(a);
+
+        mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
+    }
+    public void startMotor(int velocity){
+        byte v = (byte)(-velocity < 0 ? (255 - velocity) : -velocity);
+        byte[] a = new byte[]{8, 0x00, (byte)0x81, 0x01, 0x11, 0x51, 0x00, v};
+        mBluetoothGattCharacteristic.setValue(a);
+        mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
+    }
+    public void stopMotor(){
+        byte[] a = new byte[]{8, 0x00, (byte)0x81, 0x01, 0x11, 0x51, 0x00, 0x00};
+        mBluetoothGattCharacteristic.setValue(a);
+        mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
+    }
+    public void rotateToAngle(int angle){
+        angle = normalizeAngle(angle);
+        byte[] a = new byte[]{0x0E, 0x00, (byte)0x81, 0x00, 0x11, 0x0D, (byte)(angle & 0xff), (byte)((angle >> 8) & 0xff), (byte)((angle >> 16) & 0xff), (byte)((angle >> 24) & 0xff), 0x32, 0x32, 0x7E, 0x00};
+        mBluetoothGattCharacteristic.setValue(a);
+        mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
+    }
+    public void restoreAngle(){
+        byte[] a = new byte[]{0x0E, 0x00, (byte)0x81, 0x00, 0x11, 0x0D, 0x00, 0x00, 0x00, 0x00, 0x32, 0x32, 0x7E, 0x00};
+        mBluetoothGattCharacteristic.setValue(a);
+        mGatt.writeCharacteristic(mBluetoothGattCharacteristic);
     }
 }
